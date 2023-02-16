075657fee8d5b5b201f3afc1086e925b4783d0fa
CapraCoding <113940238+CapraCoding@users.noreply.github.com>
2023-02-15 21:10:38 -0300

    Delete plugins directory

--- plugins/airflow_plugin.py
+++
@@ -1,8 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-from operators.twitter_operator import TwitterOperator
-from operators.alfa_operator import AlOperator
-
-#change the name
-class CapraAirflowPlugin(AirflowPlugin):
-    name = "capra"
-    operators = [TwitterOperator, AlOperator]
