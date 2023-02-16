73926d5e8ebc9b02167fbb5ad181b3f1ef64392c
CapraCoding <113940238+CapraCoding@users.noreply.github.com>
2023-02-15 21:11:07 -0300

    Delete datapipeline/airflow/dags/plugins directory

--- datapipeline/airflow/dags/plugins/airflow_plugin.py
+++
@@ -1,8 +0,0 @@
-from airflow.plugins_manager import AirflowPlugin
-from operators.twitter_operator import TwitterOperator
-from operators.alfa_operator import AlfaOperator
-
-
-class CapraAirflowPlugin(AirflowPlugin):
-    name = "capra"
-    operators = [TwitterOperator, AlfaOperator]
